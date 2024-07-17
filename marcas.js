a23d959dbd5c0665326f50e065126b9d65d8456c
Ariellibra <ariel-plate@hotmail.com>
2024-07-17 19:30:07 -0300

    marcas sin grid, el resto ok

--- marcas.js
+++ marcas.js
@@ -1,31 +1,30 @@
 let imagenes = [
   {
       "url": "toyota-logo-2.png",
-      "nombre": "Proyecto 01",
-      "descripcion": "Este es el proyecto 01 fue desarrollado para un video de youtube. Si te gusta el contenido dale like y suscribete"
-
+      "nombre": "Toyota",
+      "descripcion": "Fundada en 1937, Toyota se convierto en una de las Empresas Niponas mas destacadas en la epoca de los 90', lanzando autos iconicos como el AE86, el Celica o su aclamado MK4 Supra."
   },
   {
       "url": "nissan-logo-4.jpg",
-      "nombre": "Proyecto 02",
-      "descripcion": "Hola a todos este es el proyecto02 y fue desarrollado para un video de youtube. Si te gusta el contenido dale like y suscribete"
+      "nombre": "Nissan",
+      "descripcion": "Los creadores del aclamado R-32, o mejor conocido como 'Godzilla' en el occidente, una compañia que a dia de hoy sigue haciendo autos deportivos de Alta Gamma."
 
   },
   {
       "url": "mitsubishi-logo-3.jpg",
-      "nombre": "Proyecto 03",
-      "descripcion": "Este proyecto, es el 03 y fue desarrollado para un video de youtube. Si te gusta el contenido dale like y suscribete"
+      "nombre": "Mitsubishi",
+      "descripcion": "Una marca que vive de la gloria de su pasado, Mitsubishi fue capaz de dominar los circuitos de Rally gracias a su inovacion, el apodado Lancer Evolution I, fue el primero de muchos por venir, que gracias a su traccion de cuatro ruedas dominaron los circuitos."
 
   },
   {
     "url": "subaru-logo-2.jpg",
-    "nombre": "Proyecto 04",
-    "descripcion": "Este proyecto, es el 03 y fue desarrollado para un video de youtube. Si te gusta el contenido dale like y suscribete"
+    "nombre": "Subaru",
+    "descripcion": "La reconocida marca de color azul, destacada por ser rival de Mitsubishi en los circuitos de rally, el Subaru Impreza WRX STI es uno de los grandes itos de la generacion de rally en su epoca, vistiendo su color azul y dorado, peleaba cabeza a cabeza con el Evo."
   },
   {
     "url": "mazda-logo-1.png",
-    "nombre": "Proyecto 05",
-    "descripcion": "Este proyecto, es el 03 y fue desarrollado para un video de youtube. Si te gusta el contenido dale like y suscribete"
+    "nombre": "Mazda",
+    "descripcion": "Una marca poco reconocida, pero que se destaco por su 'inovador' motor rotativo, el 13B-Rew, capaz de llevar su pequeño piston tringualar a revoluciones por encima de las 7000rpm, creadores del Rx7 y el Miata, autos deportivos de gran renombre en los 90."
   },
 ]
 
